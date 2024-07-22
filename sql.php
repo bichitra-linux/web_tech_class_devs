886866781e1af125cfd002a51cd2f820eee51639
Bichitra Gautam <bichitragautam25@gmail.com>
2024-07-22 08:44:30 +0545

    chore: Update SQL password to empty string

--- sql.php
+++ sql.php
@@ -3,7 +3,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password ="password";
+$password ="";
 $dbname = "myDB";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
